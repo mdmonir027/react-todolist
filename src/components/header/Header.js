8dcbe1930a6ea2e8c3c8caf016059e08ca3dce38
Monirul Islam <Monirul Islam>
2022-09-06 19:17:21 +0600

    init with header design

---
+++ src/components/header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className='container'>
+        <h2 className='header__text'>Simple Todo Application with Redux</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
